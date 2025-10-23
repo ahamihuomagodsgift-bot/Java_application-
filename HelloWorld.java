832ce1af0a269c9c270ec7fb926396a8498b3794
AHAMIHUOMA GODSGIFT <ahamihuomagodsgift@gmail.com>
2025-10-23 12:07:42 +0100

    Typecasting

---
+++ HelloWorld.java
@@ -0,0 +1,33 @@
+public class helloworld {
+	 public static void main(String[] agrgs){
+	    System.out.println("Hello World,This Is My first Java program");
+		System.out.println("learning java is fun");
+		System.out.print("The print method should be used to accept input");
+	    System.out.print(" Another Print Method");
+		
+	 } 
+	 
+	    int num = 50;
+		String name = "Jack";
+		Float deci = 9.99999F;
+		boolean isFun = true;
+		char symbol = '?';
+		
+		//using println method
+		System.out.println("Hello World, This is my First Java Program");
+		System.out.pritln ("Learning java is fun");
+		
+		
+	    //using  pritln method
+		System.out.print("The print method should be used to accept input ");
+		System.out.print ("Another print Method \n");
+		
+		//using println method
+		System.out.printf("I bought %d quantity of bread%n",num);
+		System.out.printf("My name is %s%n",name);
+	    System.out.printf("Your balance is %c%.2f",Symbol,deci);
+        System.out.Printf("Is Java Fun? %b",isFun);
+		System.out.print("Do you enjoy learning \"Java\""); 
+		
+		
+} 		  
